af03bcde282cb068e8f1ef04654da23e7575150b
Robyn <77750040+rpg0001@users.noreply.github.com>
2024-02-25 22:07:01 +0000

    reformat navbar

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function NavBar() {
     return (
         <nav className="navbar">
-            <Link href="/"><h1 className="m-0 text-white">Flag Quiz</h1></Link>
+            <Link href="/"><h1 className="m-0 mx-6 text-white">Flag Quiz</h1></Link>
             <Link className="navbar-link" href="/">Home</Link>
             <Link className="navbar-link" href="all-countries">All countries</Link>
         </nav>
